b5a7489b466edc29f16e030919e18acebe90ddf2
calderon <calderon@cern.ch>
2024-01-08 16:53:20 +0100

    first commit

--- Full2018_v7/condor/darkHiggs2018_v7/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc11-opt/setup.sh
-source /afs/cern.ch/user/c/calderon/work/private/mkShapesRDF_run2/mkShapesRDF/myenv/bin/activate
-time python runner.py
-cp output.root /afs/cern.ch/work/c/calderon/private/mkShapesRDF_run2/mkShapesRDF/works/DarkHiggs_RDF/Full2018_v7/rootFiles__darkHiggs2018_v7/mkShapes__darkHiggs2018_v7__ALL__${1}.root
-rm output.root
-rm script.py
