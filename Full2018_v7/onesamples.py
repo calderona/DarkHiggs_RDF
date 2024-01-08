d7195b2bc4e6b758369d1355430ad06b0b9d8f22
calderon <calderon@cern.ch>
2024-01-08 16:43:09 +0100

    first commit

---
+++ Full2018_v7/onesamples.py
@@ -0,0 +1,110 @@
+import os
+import inspect
+
+from mkShapesRDF.lib.search_files import SearchFiles
+
+searchFiles = SearchFiles()
+redirector = ""
+
+
+################################################
+################# SKIMS ########################
+################################################
+
+mcProduction = 'Autumn18_102X_nAODv7_Full2018v7'
+dataReco = 'Run2018_102X_nAODv7_Full2018v7'
+fakeReco = 'Run2018_102X_nAODv7_Full2018v7'
+mcSteps = 'MCl1loose2018v7__MCCorr2018v7__l2loose__l2tightOR2018v7{var}'
+fakeSteps = 'DATAl1loose2018v7__l2loose__fakeW'
+dataSteps = 'DATAl1loose2018v7__l2loose__l2tightOR2018v7'
+
+samples = {}
+
+##############################################
+###### Tree base directory for the site ######
+##############################################
+
+##############################################
+###### Tree base directory for the site ######
+##############################################
+
+treeBaseDir = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano'
+limitFiles = -1
+
+def makeMCDirectory(var=''):
+    return os.path.join(treeBaseDir, mcProduction, mcSteps.format(var=''))
+
+
+mcDirectory = makeMCDirectory()
+fakeDirectory = os.path.join(treeBaseDir, fakeReco, fakeSteps)
+dataDirectory = os.path.join(treeBaseDir, dataReco, dataSteps)
+
+
+def nanoGetSampleFiles(path, name):
+    _files = searchFiles.searchFiles(path, name, redirector=redirector)
+    if limitFiles != -1 and len(_files) > limitFiles:
+        return [(name, _files[:limitFiles])]
+    else:
+        return [(name, _files)]
+
+
+def CombineBaseW(samples, proc, samplelist):
+    _filtFiles = list(filter(lambda k: k[0] in samplelist, samples[proc]["name"]))
+    _files = list(map(lambda k: k[1], _filtFiles))
+    _l = list(map(lambda k: len(k), _files))
+    leastFiles = _files[_l.index(min(_l))]
+    dfSmall = ROOT.RDataFrame("Runs", leastFiles)
+    s = dfSmall.Sum("genEventSumw").GetValue()
+    f = ROOT.TFile(leastFiles[0])
+    t = f.Get("Events")
+    t.GetEntry(1)
+    xs = t.baseW * s
+
+    __files = []
+    for f in _files:
+        __files += f
+    df = ROOT.RDataFrame("Runs", __files)
+    s = df.Sum("genEventSumw").GetValue()
+    newbaseW = str(xs / s)
+    weight = newbaseW + "/baseW"
+
+    for iSample in samplelist:
+        addSampleWeight(samples, proc, iSample, weight)
+
+
+def addSampleWeight(samples, sampleName, sampleNameType, weight):
+    obj = list(filter(lambda k: k[0] == sampleNameType, samples[sampleName]["name"]))[0]
+    samples[sampleName]["name"] = list(
+        filter(lambda k: k[0] != sampleNameType, samples[sampleName]["name"])
+    )
+    if len(obj) > 2:
+        samples[sampleName]["name"].append(
+            (obj[0], obj[1], obj[2] + "*(" + weight + ")")
+        )
+    else:
+        samples[sampleName]["name"].append((obj[0], obj[1], "(" + weight + ")"))
+
+
+
+
+
+
+#########################################
+############ MC COMMON ##################
+#########################################
+
+mcCommonWeightNoMatch = 'XSWeight*SFweight*METFilter_MC'
+mcCommonWeight = 'XSWeight*SFweight*PromptGenLepMatch2l*METFilter_MC'
+
+###########################################
+#############  BACKGROUNDS  ###############
+###########################################
+
+
+###### WW ########
+
+samples['WW'] = {
+    'name': nanoGetSampleFiles(mcDirectory, 'WWTo2L2Nu'),
+    'weight': mcCommonWeight + '*nllW',
+    'FilesPerJob': 3
+}
---
+++ Full2018_v7/variables.py
@@ -0,0 +1,7 @@
+variables = {}
+
+variables['mll']  = {   'name': 'mll',
+                        'range' : (40, 20., 100.),
+                        'xaxis' : 'm_{ll} [GeV]',
+                        'fold' : 3
+                        }
